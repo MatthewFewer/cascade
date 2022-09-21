9d4c025dc1915517aa3a3cd0068fadaa8e2e7a9d
MatthewFewer <113919348+MatthewFewer@users.noreply.github.com>
2022-09-21 11:15:39 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-	
+    let x = 0
+    for (let y = 0; y <= x; y++) {
+        for (let index = 0; index <= 4; index++) {
+        	
+        }
+    }
 })
